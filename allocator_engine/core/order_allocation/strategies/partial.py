3051302d35536d19cd53c973200a2ee7f1d7befc
Yadnyesh2003 <yfirke2003@gmail.com>
2026-01-13 15:44:31 +0530

    One more change in prev commit

--- allocator_engine/core/order_allocation/strategies/partial.py
+++ allocator_engine/core/order_allocation/strategies/partial.py
@@ -1,22 +1,10 @@
 
 
 
-class PartialOrderAllocator(BaseAllocator):
+from allocator_engine.core.order_allocation.base_order_allocator import BaseOrderAllocator
+
+
+class PartialOrderAllocator(BaseOrderAllocator):
     """
     Allocates orders partially based on available resources.
     """
-
-    def allocate(self, order, resources):
-        allocated = {}
-        remaining_quantity = order.quantity
-
-        for resource in resources:
-            if remaining_quantity <= 0:
-                break
-
-            allocatable_quantity = min(resource.available_quantity, remaining_quantity)
-            if allocatable_quantity > 0:
-                allocated[resource.id] = allocatable_quantity
-                remaining_quantity -= allocatable_quantity
-
-        return allocated
